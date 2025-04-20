5d27604cc0bdbdb55be7d25885ad90471633db09
Jacque-Js <nick.nick13505@mail.ru>
2025-04-20 22:31:25 +0700

    Add files

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: "/hangman-game/",  // Для GitHub Pages
-  root: '.',               // Корень проекта (по умолчанию и так ".")
-  publicDir: 'public',     // Папка со статикой
-});
