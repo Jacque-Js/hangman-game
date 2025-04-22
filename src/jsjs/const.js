836be0eda96a4a7ba4dc1f74abb69b5077a326f3
Jacque-Js <nick.nick13505@mail.ru>
2025-04-22 19:41:12 +0700

    Fix bags

--- src/jsjs/const.js
+++ src/jsjs/const.js
@@ -1,4 +1,4 @@
-export const WORDS = ['code']
+export const WORDS = ['code', 'programming', 'play', 'games', 'home', 'dog', 'cat', 'pig', 'teacher', 'soccer', 'runner', 'computer', 'telescope', 'elephant', 'butterfly', 'sandwich', 'mountain', 'apple']
 
 export const KEYBOARD_LETTERS = Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
 
